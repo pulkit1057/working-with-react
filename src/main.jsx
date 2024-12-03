ccca2aecf1e02ddbd6887f0ec89d896828aa90e5
pulkit1057 <100367696+pulkit1057@users.noreply.github.com>
2024-12-03 21:58:17 +0530

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,15 @@ import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <div>
-    <img src="src/assets/react_logo.png" alt="" />
-    <h1>Hello there</h1>
-  </div>
+  <main>
+    <img src="src/assets/react_logo.png" width="60px"/>
+    <h1>Fun facts about React!</h1>
+    <ul>
+      <li>Was first release in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 200k stars on GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </main>
 )
