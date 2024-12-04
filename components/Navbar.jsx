4ff13f214f5b08baaaf2aef16ed48aca9d74f5aa
pulkit1057 <100367696+pulkit1057@users.noreply.github.com>
2024-12-04 15:11:59 +0530

    forth commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return (
+        <header className="header">
+            <nav>
+                <img src="src/assets/react_logo1.png" alt="React Logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-import { createElement } from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client"
+import App from "./App"
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <main>
-    <img src="src/assets/react_logo.png" width="60px"/>
-    <h1>Fun facts about React!</h1>
-    <ul>
-      <li>Was first release in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 200k stars on GitHub</li>
-      <li>Is maintained by Meta</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </main>
+  <>
+    <App />
+  </>
 )
